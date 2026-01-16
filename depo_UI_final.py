3c82ed3b2e4d77e1c448a7cb6a8758fecee97324
Tejashwini-8873 <tejashwini.k@wonderful.com>
2026-01-16 22:44:44 +0530

    check

--- depo_UI_final.py
+++ depo_UI_final.py
@@ -256,7 +256,7 @@ def extract_page_group_json(text: str):
     raise ValueError("No valid Page-Group JSON found in GPT output")
 
 
-executor = ThreadPoolExecutor(max_workers=4)
+executor = ThreadPoolExecutor(max_workers=2)
 def create_deposition_summary(input_docx, output_docx):
     # Load input DOCX
     doc = Document(input_docx)
@@ -1225,4 +1225,5 @@ st.markdown("""
     © The Wonderful Company LLC 🌳 All Rights Reserved.
 </div>
 """, unsafe_allow_html=True)
+
 
